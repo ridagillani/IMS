9edcd1aa730618cd41a749604a5cef90e07dba52
ridagillani <ridaenoorgillani@gmail.com>
2021-12-31 16:10:56 +0500

    jn

---
+++ src/ims/Orders.java
@@ -0,0 +1,58 @@
+package ims;
+
+public class Orders {
+
+    int product_id;
+    int order_number;
+    int quantity;
+    double amount;
+
+    public Orders()
+    {
+        product_id = 0;
+        order_number = 0;
+        quantity = 0;
+        amount = 0.0;
+    }
+
+    public void setID(int product_id)
+    {
+        this.product_id = product_id;
+    }
+
+    public int getID()
+    {
+        return product_id;
+    }
+
+    public void setOrder(int order)
+    {
+        this.order_number = order_number;
+    }
+
+    public int getOrder()
+    {
+        return order_number;
+    }
+
+    public void setQuantity(int quantity)
+    {
+        this.quantity = quantity;
+    }
+
+    public int getQuantity()
+    {
+        return quantity;
+    }
+
+    public void setAmount(double amount)
+    {
+        this.amount = amount;
+    }
+
+    public double getAmount()
+    {
+        return amount;
+    }
+
+}
