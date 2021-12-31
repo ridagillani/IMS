99e925421d5530bd146076f58dd7586d1c101cbf
ridagillani <ridaenoorgillani@gmail.com>
2021-12-31 16:24:06 +0500

    ji

--- src/ims/Product.java
+++ src/ims/Product.java
@@ -10,7 +10,8 @@ public class Product {
     private String category;
     private double price;
 
-    public Product(){
+    public Product()
+    {
         id = 0;
         name = null;
         description = null;
@@ -18,7 +19,9 @@ public class Product {
         category = null;
         price = 0.0;
     }
-    public Product(int id, String name, String description, int quantity, String category, double price){
+
+    public Product(int id, String name, String description, int quantity, String category, double price)
+    {
         this.id = id;
         this.name = name;
         this.description = description;
@@ -27,6 +30,64 @@ public class Product {
         this.price = price;
     }
 
+    public void setPID(int id)
+    {
+        this.id = id;
+    }
 
+    public int getPId()
+    {
+        return id;
+    }
+
+    public void setPname(String name])
+    {
+        this.name = name;
+    }
+
+    public String getPname()
+    {
+        return name;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setPQuantity(int quantity)
+    {
+        this.quantity = quantity;
+    }
+
+    public int getPQuantity()
+    {
+        return quantity;
+    }
+
+    public void setCategory(String category)
+    {
+        this.category = category;
+    }
+
+    public String getCategory()
+    {
+        return category;
+    }
+
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
 
 }
